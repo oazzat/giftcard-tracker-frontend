e243107c120c41854c54b5492d184a1c2577d789
oazzat <oazzat19@gmail.com>
2019-02-23 21:46:58 -0500

    took out rexux dev tools

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ import thunk from 'redux-thunk'
 
 // const reducers = combineReducers({mainReducer})
 
-const store = createStore(mainReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ ))
-// && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(mainReducer,applyMiddleware(thunk))
+// compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 
 ReactDOM.render(
   <Provider store={store}>
